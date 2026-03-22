70d410547364906ea37c565255dc8dcd4c4654c4
Xyno-dev <zayanarshad88@gmail.com>
2026-03-22 19:37:55 +0000

    feat: made the webpage a pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({plugins: [
+    tailwindcss(), sveltekit(), SvelteKitPWA({
+      manifest: {
+        name: 'SigmaControl',
+        short_name: 'Control',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+      }
+    })
+]});
