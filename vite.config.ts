987c1a211b770829eedd2eaac34896431592fa12
Xyno-dev <zayanarshad88@gmail.com>
2026-03-22 22:37:39 +0000

    fix: changed the output directory for sveltekitpwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({plugins: [
     tailwindcss(), sveltekit(), SvelteKitPWA({
+      outDir: '.svelte-kit/output/client',
       registerType: 'autoUpdate',
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}']
